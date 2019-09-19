d64f16b0ddc33b91e9dc78d8c47e0668c0f927d5
Amir Behmanesh <amirrezabsh79@gmail.com>
2019-09-19 13:13:55 +0430

    Search icon added to the action bar.

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
         drawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
 getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_button,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
